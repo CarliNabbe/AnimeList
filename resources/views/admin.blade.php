523bc78e619f83b521f7f3f9f38349d657645228
CarliNabbe <carli_nabbe@hotmail.com>
2019-10-28 13:15:01 +0100

    Added admin login and admin user

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ADMIN Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <h1>Your own posts</h1>
+                    <h1>You are logged in as admin</h1>
+                    <li>
+                        <a class="btn btn-primary" style="margin: 10px;" href="/posts/create">Create Post</a>
+                    </li>
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a></td>
+                            <td>
+                                {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'DELETE', 'class' => 'float-right']) !!}
+                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+    Route::prefix('admin')->group(function() {
+        Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+        Route::get('/login', 'Auth\AdminLoginController@login')->name('admin.submit');
+        Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    });
+
+
 Route::get('/post/toggleLike/{id}', 'PostsController@toggleLike')->name('toggleLike');
